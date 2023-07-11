a9e7785a02a1514561fa9e6bed525037939d8b15
JinSo <kimjinso@qq.com>
2023-07-11 20:05:00 +0800

    feat:  用户音视频状态显示

--- client/components/Room/Exhibition/LocalMedia.tsx
+++ client/components/Room/Exhibition/LocalMedia.tsx
@@ -19,7 +19,8 @@ const LocalMedia = ({ mediaType, me, publishAudio, publishVideo, publishShare, c
 	const audioRef = useRef<HTMLAudioElement>(null)
 	const [hasAudio, setHasAudio] = useState(mediaType === MediaType.AUDIO || mediaType === MediaType.ALL)
 	const [hasVideo, setHasVideo] = useState(mediaType === MediaType.VIDEO || mediaType === MediaType.ALL)
-	const [hasShare, setHasShare] = useState(false)
+  const [hasShare, setHasShare] = useState(false)
+	const videoEnabled = !!me.producers.video
 	const styles = {
 		audioColorScheme: hasAudio ? 'teal' : 'gray',
 		audioImg: hasAudio ? '/img/audio.svg' : '/img/audio_forbid.svg',
@@ -65,14 +66,16 @@ const LocalMedia = ({ mediaType, me, publishAudio, publishVideo, publishShare, c
 	return (
 		<div className="relative bg-[rgba(49,49,49,0.9)] hover:shadow-[0_0_8px_rgba(82,168,236,0.9)] rounded-lg">
 			{/* 音视频 */}
-			<div className="flex justify-center items-end w-[488px] h-[274.5px] select-none">
-				<video ref={videoRef} playsInline autoPlay className="h-full object-fill" muted />
+			<div
+				className={`flex justify-center items-end w-[488px] h-[274.5px] select-none ${videoEnabled ? '' : 'invisible'}`}
+			>
+				<video ref={videoRef} playsInline autoPlay muted className="h-full object-fill" />
 				<audio ref={audioRef} autoPlay className="invisible" muted />
 			</div>
 			{/* 如果没开视频时，显示头像 */}
 			<div
-				className={`absolute left-0 top-0 flex justify-center items-end w-[488px] h-[274.5px] select-none  bg-[rgba(49,49,49,0.9)] ${
-					hasVideo ? 'z-[-1]' : 'z-10'
+				className={`absolute left-0 top-0 flex justify-center items-end w-[488px] h-[274.5px] select-none ${
+					videoEnabled ? 'z-[-1]' : 'z-10'
 				}`}
 			>
 				<Image src={'/img/avatar.svg'} alt="avatar" width={250} height={250} />
--- client/components/Room/Exhibition/RemoteMedia.tsx
+++ client/components/Room/Exhibition/RemoteMedia.tsx
@@ -10,9 +10,9 @@ interface Props {
 const RemoteMedia = ({ peer }: Props) => {
 	const consumers = useMediasoupStore(state => state.consumers)
 	const videoRef = useRef<HTMLVideoElement>(null)
-	const audioRef = useRef<HTMLAudioElement>(null)
-
-  console.log(peer, consumers)
+  const audioRef = useRef<HTMLAudioElement>(null)
+	const audioEnabled = peer.consumers.audio
+	const videoEnabled = peer.consumers.video
 
 	// 监听变化再渲染
 	useEffect(() => {
@@ -33,17 +33,33 @@ const RemoteMedia = ({ peer }: Props) => {
 	return (
 		<div className="relative bg-[rgba(49,49,49,0.9)] hover:shadow-[0_0_8px_rgba(82,168,236,0.9)] rounded-lg">
 			{/* 音视频 */}
-			<div className="flex justify-center items-end w-[488px] h-[274.5px] select-none">
-				<video ref={videoRef} playsInline autoPlay className="h-full object-fill" />
+			<div
+				className={`flex justify-center items-end w-[488px] h-[274.5px] select-none ${videoEnabled ? '' : 'invisible'}`}
+			>
+				<video
+					ref={videoRef}
+					playsInline
+					autoPlay
+					className={`h-full object-fill ${videoEnabled ? '' : 'invisible'}`}
+				/>
 				<audio ref={audioRef} autoPlay className="invisible" />
 			</div>
 			{/* 如果没开视频时，显示头像 */}
-			<div className={`absolute left-0 top-0 flex justify-center items-end w-[488px] h-[274.5px] select-none`}>
+			<div
+				className={`absolute left-0 top-0 flex justify-center items-end w-[488px] h-[274.5px] select-none ${
+					videoEnabled ? 'z-[-1]' : 'z-10'
+				}`}
+			>
 				<Image src={'/img/avatar.svg'} alt="avatar" width={250} height={250} />
 			</div>
 			{/* 用户名 */}
 			<div className="absolute bottom-2 left-2 bg-[#252525] text-white border-b-2 border-b-[#aeff00] select-none text-sm p-[4.8px]">
 				<p>{peer.username}</p>
+			</div>
+			{/* 音视频状态 */}
+			<div className="absolute top-2 right-2 select-none flex gap-4">
+				{!audioEnabled && <Image src={'/img/audio_forbid.svg'} alt="audio" width={18} height={18} />}
+				{!videoEnabled && <Image src={'/img/video_forbid.svg'} alt="audio" width={18} height={18} />}
 			</div>
 		</div>
 	)
